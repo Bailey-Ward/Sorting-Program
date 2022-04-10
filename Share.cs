b80416e8cac17ea8ddb18c7aa53de4537aad25d8
FirstClassGravy <41171656+FirstClassGravy@users.noreply.github.com>
2022-04-10 14:58:51 +0100

    start

---
+++ Share.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Program
+{
+	public class Share
+	{
+		int[] sharesList = new shareList[] { };
+		string strShares = "";
+		int intShares;
+
+		public List<string> Shares()
+		{
+			try
+			{
+				string strShares = File.ReadAllText("Share_1_256.txt")
+				bool isParsable = int32.TryParse(strShares, out intShares);
+
+				if 
+					{
+					(isParsable)
+						this.sharesList.Add(intshares);
+					return sharesList;
+				}
+
+				else
+					{ 
+					Console.WriteLine("Could not be parsed.");
+					}
+			}
+			catch
+            {
+				Console.WriteLine("File could not be found");
+				Environment.Exit(0);
+				return null;
+            }
+		}
+	}
+}
---
+++ Sort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Program
+{
+	public class Sort
+	{
+		public bubbleSort()
+		{
+
+		}
+
+		public mergeSort()
+		{
+
+		}
+
+		public quickSort()
+		{
+
+		}
+	}
+}
